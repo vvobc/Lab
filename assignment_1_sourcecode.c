56cde375e9fe7e8feced7db877dc205a2e5fb4bd
vvobc <chanida.bo@mail.kmutt.ac.th>
2018-03-23 20:37:45 +0700

    First copy 3 files

---
+++ assignment_1_sourcecode.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#include <string.h>
+#define MAX 8
+int Binary(int data[],char text[])
+{
+    int x,i;
+    char str[200];
+    do
+    {
+        x=0;
+        printf("%s ",text);
+        scanf("%s",str);
+        if(strlen(str)>8)                                           //>>>    Check number not more 8 bits
+            x=1;                                                    //>>>    x=1 More than 8 bits
+        else
+        {
+            for(i=0;i<strlen(str);i++)                              //\*
+            {                                                       // \*    Move data(char) into data(int)
+                data[i+(MAX-strlen(str))]=(str[i]-48);              //  \*   and Check number is 0 1 only
+                if(str[i]!='0'&&str[i]!='1')                        //  /    x=2 not only 0 1
+                    x=2;                                            // /
+            }                                                       ///
+        }
+        if(x==1)
+            printf("More than 8 bits \n");
+        else if(x==2)
+            printf("Use only binary numeral system ( 0 1 )\n");
+    }while(x!=0);
+}
+int plus(int data1[],int data2[],int output[],int buff[])
+{
+ int i,y=0;                                         // First carry bit =0 (y=0)
+    buff[MAX-1]=0;                                  // Function + Binary number
+    for(i=MAX-1;i>=0;i--)                           // y is carry bit,
+    {                                               // y=0 Binary number = 0
+        y+=(data1[i]+data2[i]);                     // y=1 Binary number = 1
+        if(y>1)                                     // y=2 Binary number = 0
+        {                                           // y=3 Binary number = 1
+            if(y==2)                                // if y>1 set new carry bit = 1
+                output[i]=0;
+            else
+                output[i]=1;
+            y=buff[i-1]=1;
+        }
+        else
+        {
+            if(y==0)
+                output[i]=0;
+            else
+                output[i]=1;
+            y=buff[i-1]=0;
+        }
+    }
+}
+int OneComp(int data[])
+{
+    int i;
+    for(i=0;i<MAX;i++)
+        data[i] = (data[i]==1)? 0:1;                            //>>>  switching 0 <-> 1
+}
+int Show(int data[])
+{
+    int Ten=0,i,y;
+    for(i=0;i<MAX;i++)
+        printf("%d",data[i]);                                   // Print Binary number
+    for(y=MAX-1,i=0;i<MAX;i++,y--)                              // and number base 10
+    {                                                           //
+        Ten+=data[i]*(int)pow(2,y);                             // Calculations Number base 10
+    }
+    if(data[0]==1)                                              //
+        Ten-=256;                                               //
+    printf("   (%d)",Ten);                                      //
+    printf("\n");                                               //
+}
+int main()
+{
+    int i,DATA[MAX]={0,0,0,0,0,0,0,0},DATA2[MAX]={0,0,0,0,0,0,0,0};
+    int OUTPUT[MAX]={0,0,0,0,0,0,0,0},BUFF[MAX],ONE[MAX]={0,0,0,0,0,0,0,1};
+    char op,ch;
+    Binary(DATA,"First");                                           //>>>> Input First binary number
+
+    printf("Enter Operators + or - only ");                         //\*
+    while(scanf(" %c%c",&op,&ch)!=2||(op!='+'&&op!='-')||ch!='\n')  // \*
+    {                                                               //  \*   Input Operator , + - only
+        printf(" + or - Only\n");                                   //  /    Defense input values other
+        rewind(stdin);                                              // /
+    }                                                               ///
+
+    Binary(DATA2,"Second");                                         //>>>> Input Second binary number
+
+    if(op=='-')                                                     //\*    Function OneComp (1's Complement)
+    {                                                               // \*    is change 1 -> 0, 0 -> 1
+        OneComp(DATA2);                                             //  \*
+        plus(DATA2,ONE,OUTPUT,BUFF);                                //   \*   Function Plus is 1's Complement + 1 or 2's Complement
+        for(i=0;i<MAX;i++)                                          //   /
+            DATA2[i]=OUTPUT[i];                                     //  /
+    }                                                               ///
+
+    plus(DATA,DATA2,OUTPUT,BUFF);                                   //>>>>  First Number + Second Number
+
+    //system("cls");                                                  //>>>>  Clear screen
+
+    for(i=0;i<MAX;i++)                                              //\*
+    {                                                               // \*
+        if(BUFF[i]==1)                                              //  \*
+            printf("%d",BUFF[i]);                                   //      Show carry bit
+        else                                                        //  /
+            printf(" ");                                            // /
+    }                                                               ///
+    printf("\n");
+    Show(DATA);                                                     //>>>>  Show Number(1) ex. 1010 (10)
+    printf("         +\n");
+    Show(DATA2);                                                    //>>>>  Show Number(2) ex. 1010 (10)
+    printf("--------\n");
+    if((DATA[0]==DATA2[0]&&((DATA[0]==0&&OUTPUT[0]==0)||(DATA[0]==1&&OUTPUT[0]==1)))||DATA[0]!=DATA2[0])  //>>>> If the first(index[0]) is equal to zero. Value is positive only ถ้าตัวแรกของเลขเป็นเลข0 เลขฐาน2 มีค่าอยู่ในโซนบวก
+        Show(OUTPUT);//>>>>  Show Answer ex. 1010 (10)                                                    //>>>> If the first(index[0]) is equal to one . Value is Negative only ถ้าตัวแรกของเลขเป็นเลข1 เลขฐาน2 มีค่าอยู่ในโซนลบ
+    else                                                                                                  //>>>> So positive(DATA) + positive(DATA2) Must equal positive only    จำนวนบวก+จำนวนบวกจะได้จำนวนบวก
+        printf("Overflow\n");                                                                             //>>>> And Negative + Negative Must= Negative only                     จำนวนลบ+จำนวนลบจะได้จำนวนลบ
+    printf("========\n");                                                                                 //o>>> if positive+positive!=positive or Negative + Negative != Negative ,then Overflow ถ้าไม่เป็นตามนั้น แปลว่าเกิดการ  Overflow
+}
---
+++ assignment_2_sourcecode.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#include <string.h>
+#define MAXBITS 32
+int main()
+{
+    double DecimalPoint,Buff;
+    int i,j,Expo,Decimal,DataBuff[MAXBITS],Output[MAXBITS];
+    char ch;
+    while(scanf("%lf%c",&Buff,&ch)!=2||ch!='\n')    //>>>>> Input real numbers
+        rewind(stdin);
+    if(Buff>=0)                                     //>>>>> 0 Positive numbers
+        Output[0]=0;
+    else
+    {
+        Output[0]=1;                                //>>>>> 1 Negative numbers
+        Buff*=-1;
+    }
+    Decimal=Buff;                                   //>>>>> Decimal is integer
+    DecimalPoint=Buff-Decimal;                      //>>>>> DecimalPoint is Decimal Point
+    if(Decimal>0)
+    {
+        for(i=MAXBITS-1;i>=0;i--)                   //>>>>> Converter Decimal to Binary (integer)
+        {
+            DataBuff[i]=Decimal%2;
+            Decimal/=2;
+        }
+        for(i=0;DataBuff[i]!=1;i++){}               //>>>>> Move points
+        Expo=MAXBITS-i+1022;
+        for(j=11;j>=1;j--)                          //>>>>> Converter Decimal to Binary (Exponent)
+        {
+            Output[j]=Expo%2;
+            Expo/=2;
+        }
+        for(j=12,i=i+1;i<MAXBITS;i++,j++)           //>>>>> Copy Binary (Decimal Point) into Output *Start index at 12
+           Output[j]=DataBuff[i];
+        for(j=j;j<MAXBITS;j++)                      //>>>>> Converter Decimal to Binary (Decimal Point)
+        {
+            DecimalPoint*=2;
+            if(DecimalPoint>=1)
+            {
+                Output[j]=1;
+                DecimalPoint-=1;
+            }
+            else
+                Output[j]=0;
+        }
+    }
+    else
+    {
+        for(i=0;i<MAXBITS;i++)                      //>>>>> Converter Decimal to Binary (Decimal Point)
+        {
+            DecimalPoint*=2;
+            if(DecimalPoint>=1)
+            {
+                DataBuff[i]=1;
+                DecimalPoint-=1;
+            }
+            else
+                DataBuff[i]=0;
+        }
+        for(i=0;DataBuff[i]!=1;i++){}               //>>>>> Move points
+        Expo=1022-i;
+        for(j=11;j>=1;j--)                          //>>>>> Converter Decimal to Binary (Exponent)
+        {
+            Output[j]=Expo%2;
+            Expo/=2;
+        }
+        for(j=12,i=i+1;j<MAXBITS;i++,j++)           //>>>>> Copy Binary (Decimal Point) into Output *Start index at 12
+        {
+            if(i<MAXBITS)
+                Output[j]=DataBuff[i];
+            else
+                Output[j]=0;
+        }
+    }
+    //printf("Sign        Exponent          Mantissa\n\n");
+    printf("%d           ",Output[0]); //print Sign
+    for(i=1;i<4;i++)
+        printf("%d",Output[i]); //print Exponent
+    for(i=4;i<12;i++)
+    {
+        if(i%4==0)
+            printf(" ");
+        printf("%d",Output[i]); //print Exponent
+    }
+    printf("    ");
+    for(i=12;i<MAXBITS;i++)
+    {
+        if(i%4==0)
+            printf(" ");
+        printf("%d",Output[i]);   //print Mantissa
+    }
+    printf("\n");
+
+    int y,Ten=0;
+    for(y=0,i=11;i>=1;i--,y++)
+    {
+        Ten+=Output[i]*(int)pow(2,y); //>>>>> Converter Binary to Decimal (Exponent)
+    }
+    Ten-=1023;
+    if(Ten<=0)
+    {
+        if(Output[0]==1)
+            printf("-0.");
+        else
+            printf("0.");
+        Ten*=-1;
+        for(i=1;i<Ten;i++)
+            printf("0");
+        printf("1");
+        for(i=i;i<MAXBITS;i++)
+            printf("%d",Output[i]);
+        printf("\n0\n0");
+    }
+    else
+    {
+        if(Output[0]==1)
+            printf("-1");
+        else
+            printf("1");
+        for(i=12;i<12+Ten;i++)
+            printf("%d",Output[i]);
+        printf(".");
+        for(i=i;i<MAXBITS;i++)
+            printf("%d",Output[i]);
+        printf("\n1");
+        for(i=12;i<12+Ten;i++)
+            printf("%d",Output[i]);
+        int Number=0;
+        for(y=0,i=11+Ten;i>=12;i--,y++)
+        {
+            Number+=Output[i]*(int)pow(2,y);
+        }
+        Number+=1*(int)pow(2,y);
+        if(Output[0]==1)
+            Number*=-1;
+        printf("\n%d",Number);
+    }
+    //printf("\n\n1 Bit       11 Bits           52 Bits\n\n");
+    //main();
+    return 0;
+}
